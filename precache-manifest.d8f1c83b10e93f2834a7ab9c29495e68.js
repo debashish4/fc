7dc8c48af1251843bb33f147eddeb686629f3bb0
debashish sahoo <debashish4@gmail.com>
2020-03-01 01:06:04 +0530

    friendship meter build file only

---
+++ precache-manifest.d8f1c83b10e93f2834a7ab9c29495e68.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab8526c06c86fbf9dc2080277a776b43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56bca18ecc8b7005df3a",
+    "url": "/static/css/main.814fffd0.chunk.css"
+  },
+  {
+    "revision": "9934b51c5016e2af9695",
+    "url": "/static/js/2.901152e0.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/static/js/2.901152e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56bca18ecc8b7005df3a",
+    "url": "/static/js/main.21d8c2b1.chunk.js"
+  },
+  {
+    "revision": "e55fe229f6f199fdc40d",
+    "url": "/static/js/runtime-main.ad1923f8.js"
+  }
+]);
